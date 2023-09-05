a39bed6b79bf2fb093560061fb94174cfc9f8e4f
Brod8362 <brod8362@gmail.com>
2023-09-05 16:26:24 -0500

    Add all ghost info

--- src/ghosts.rs
+++ src/ghosts.rs
@@ -5,9 +5,11 @@ use crate::evidence::Evidence;
 #[derive(Deserialize, Clone)]
 pub struct Ghost {
     pub name: String,
+    #[serde(alias = "speed")]
     pub min_speed: f32,
     #[serde(default)]
     pub max_speed: f32,
+    #[serde(alias = "hunt_sanity")]
     pub min_hunt_sanity: f32,
     #[serde(default)]
     pub max_hunt_sanity: f32,
