046916462c620edc0387624f2cc138836a505042
Brod8362 <brod8362@gmail.com>
2023-09-05 14:21:34 -0500

    Only allow selecting up to difficulty level

--- src/state.rs
+++ src/state.rs
@@ -16,7 +16,23 @@ impl SelectionState {
         self.evidences = 0;
     }
 
+    pub fn selected_count(self: &Self) -> u32 {
+        let mut count = 0;
+        let mut register = self.evidences;
+        while register > 0 {
+            if register & 1 == 1 {
+                count+=1;
+            }
+            register = register >> 1;
+        }
+        count
+    }
+
     pub fn mark(self: &mut Self, evidence: Evidence) {
+        //TODO add ghost orbs logic here
+        if self.selected_count() >= self.difficulty {
+            return;
+        }
         self.evidences |= evidence as u32;
     }
 
@@ -29,7 +45,11 @@ impl SelectionState {
     }
 
     pub fn toggle(self: &mut Self, evidence: Evidence) {
-        self.evidences ^= evidence as u32;
+        if self.marked(evidence) {
+            self.unmark(evidence);
+        } else  {
+            self.mark(evidence);
+        }
     }
 
     pub fn next_difficulty(self: &mut Self) {
